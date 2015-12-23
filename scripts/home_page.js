86e1e6cff103cde2a00a847cbd7d26caad426f64
kenchung <kenchung.ftd@gmail.com>
2015-12-23 20:05:55 +0800

    ver 1.0

--- scripts/home_page.js
+++ scripts/home_page.js
@@ -13,20 +13,21 @@ $(function() {
 	});
 
 	// 搜索框
+	var $searchBar = $('#search-bar');
 	$('#search-img').click(function() {
-		var $searchBar = $('#search-bar');
 		$searchBar.animate({height:'show'}, 600,'easeInOutQuint').find('a').click(function(){
 			$searchBar.animate({height:'hide'}, 600,'easeInOutQuint');
 			return false;
 		});
 	});
-	// $('#search-img :text').focus(function() {
-	// 	$(this).css('outline', 'none');
-	// });
 
 
 
 	// 轮播头图
+
+	$smallPic = $('.small-pic');
+	$bigPic = $('.big-pic');
+
 	var titles =[
 	"西甲第4轮：皇家马德里7:1希洪竞技",
 	"皇家马德里2:1击败巴塞罗那获得国王杯",
@@ -34,61 +35,98 @@ $(function() {
 	];
 
 	function movePic () {
+		if (index == 2) {index = -1};
 		index++;
 		zIndex++;
-		$('.small-pic').find('li').eq(index).addClass('nowpic').siblings().removeClass();
-		var $nowLi = $('.big-pic>ul').find('li').eq(index);
-		$nowLi.hide().css('zIndex', zIndex).show('slow');
+		$smallPic.find('li').eq(index).addClass('nowpic').siblings().removeClass();
+		$bigPic.find('li').eq(index).hide().css('zIndex', zIndex).show('slow');
 		$('.text').html(titles[index]);
-		if (index == 2) {index = -1};
-
 	}
 
 	var index = 0;
 	var zIndex = 1;
 	var timer = setInterval(movePic,4000);
-	$('.small-pic>ul').mouseover(function() {
+	$smallPic.find('ul').mouseover(function() {
 		clearInterval(timer);
 	}).mouseout(function() {
 		timer = setInterval(movePic,4000)
 	});
-	$('.small-pic li').click(function() {
-		index = parseInt($(this).index())-1;
+	$smallPic.find('li').click(function() {
+		if (index == $(this).index()) return;
+		index = $(this).index()-1;
 		movePic();
 	});
 
 
-	// 固定顶栏
+
+	// 固定导航栏
+	$nav = $('#nav');
 	$navLogo = $('.nav-logo');
 	$navLogo.hide();
 	var navState = false;
-	window.onscroll = function () {
-		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		// console.log(navState);
-		if ((scrollTop >= 117)&&(!navState)) {
-			// alert(navState);
-			$('.nav').css({
-				position: 'fixed',
-				top: 0,
-				left:0,
-				zIndex:9999,
-				borderBottom:'1px solid #ccc'
-			});
-
+	$(window).scroll(function(event) {
+		if (($(window).scrollTop() >= 117) && (!navState)) {
+			$nav.addClass('scroll-nav');
 			$navLogo.show('slow');
-
 			navState = true;
 		}
-
-		if (scrollTop < 117) {
-
-			$('.nav').css({
-				position: 'static',
-			});
-			navState = false;
+		if (($(window).scrollTop() < 117) && (navState)) {
+			$nav.removeClass();
 			$navLogo.hide();
+			navState = false;
 		}
+	});
 
-	}
+	//主内容区
+	$('.col-main article').mouseover(function() {
+		$(this).find('img').stop().animate({opacity: '0.5'}, '400');
+	}).mouseout(function() {
+		$(this).find('img').stop().animate({opacity: '1'}, '200');
+	});
+
+	//奖杯滚动
+	var cupNo = 0;
+	$gallery = $('#cups ul');
+	$cups = $gallery.find('li');
+	$gallery.css('width', $cups.length * ($cups.eq(1).width()+1) + 'px');
+	$('#cups .next').click(function() {
+		if (!$gallery.is(':animated')) {
+			if (cupNo == 0) {
+				$('#cups .prev').show();
+			}
+			$gallery.animate({left: '-=120px'}, 'fast');
+			cupNo++;
+			if (cupNo == $cups.length-8) {
+				$(this).hide();
+			}
+		}
+	});
+	$('#cups .prev').click(function() {
+		if (!$gallery.is(':animated')) {
+			if (cupNo == $cups.length-8) {
+				$('#cups .next').show();
+			}
+			$gallery.animate({left: '+=120px'}, 'fast');
+			cupNo--;
+			if (cupNo == 0) {
+				$(this).hide();
+			}
+		}
+	});
+
+	//回到顶部按钮
+	$toTOp = $('#totop');
+	$(window).scroll(function() {
+		$toTOp.stop().animate({top: $(this).scrollTop() + $(this).height() - 120 + 'px'}, 2000, 'easeOutQuint');
+		if ($(window).scrollTop() > 300) {
+			$toTOp.show();
+		} else {
+			$toTOp.hide();
+		};
+	});
+	$toTOp.click(function() {
+		$(window).scrollTop(0);
+	});
+
 
 });
